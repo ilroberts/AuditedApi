ef4c6c5fb548ac904a8b1ecb551fe523a522c802
iwan.roberts <iwan.roberts@defra.onmicrosoft.com>
2024-08-23 17:34:54 +0100

    final tidy up

--- AuditedApi/Controllers/ForecastController.cs
+++ AuditedApi/Controllers/ForecastController.cs
@@ -1,25 +1,44 @@
 using Audit.WebApi;
 using CorrelationId.Abstractions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuditedApi.Contollers
 {
+    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
     [Route("api/[controller]")]
     [ApiController]
-    public class ForecastController(ILogger<ForecastController> logger, 
+    [AuditApi(EventTypeName = "{controller}/{action} ({verb})", IncludeHeaders = true)]
+    public class ForecastController(ILogger<ForecastController> logger,
         ICorrelationContextAccessor correlationContext) : ControllerBase
     {
         private readonly ILogger<ForecastController> _logger = logger;
         private readonly ICorrelationContextAccessor _correlationContext = correlationContext;
 
+        private readonly string[] summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+            "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
+
         [HttpGet]
-        [AuditApi(EventTypeName = "GetForecasts", IncludeHeaders = true)]
-        public string GetForecasts()
+        public WeatherForecast[] GetForecasts()
         {
             var CorrelationId = _correlationContext.CorrelationContext.CorrelationId;
             _logger.LogInformation("Getting forecasts: correlationId={CorrelationId}", CorrelationId);
-            return "Forecasts";
+
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                )).ToArray();
+
+            return forecast;
         }
     }
 }
--- AuditedApi/Program.cs
+++ AuditedApi/Program.cs
@@ -1,24 +1,19 @@
-using Audit.Core;
-using Audit.Core.Providers;
 using Audit.WebApi;
 using CorrelationId;
 using CorrelationId.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDefaultCorrelationId();
 
-builder.Services.AddControllers();    
+builder.Services.AddControllers();
 builder.Services.AddMvc(options => options.Filters.Add(new AuditApiAttribute()));
 
 Audit.Core.Configuration.Setup()
     .UseFileLogProvider(cfg => cfg.Directory(@"logs"));
-//builder.Services.AddSingleton<AuditDataProvider>(dataProvider);
 
 var app = builder.Build();
 
@@ -27,7 +22,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI(c => {
+    app.UseSwaggerUI(c =>
+    {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuditedApi v1");
     });
 }
@@ -36,29 +32,4 @@ app.MapControllers();
 app.UseHttpsRedirection();
 app.UseCorrelationId();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
