f4185a58f0a042f6682a9ea42b5590390950bffe
iwan.roberts <iwan.roberts@defra.onmicrosoft.com>
2024-08-27 10:57:54 +0100

    fixed unit test

--- AuditedApi.Tests/Controllers/ForecastControllerTests.cs
+++ AuditedApi.Tests/Controllers/ForecastControllerTests.cs
@@ -1,4 +1,6 @@
 using AuditedApi.Contollers;
+using AuditedApi.Models;
+using AuditedApi.Services;
 using CorrelationId;
 using CorrelationId.Abstractions;
 using Microsoft.Extensions.Logging;
@@ -11,13 +13,17 @@ namespace AuditedApi.Controllers.Tests
         private readonly Mock<ILogger<ForecastController>> _mockLogger;
         private readonly Mock<ICorrelationContextAccessor> _mockCorrelationContextAccessor;
         private readonly ForecastController _controller;
+        private readonly Mock<IForecastService> _mockForecastService;
 
         public ForecastControllerTests()
         {
             _mockLogger = new Mock<ILogger<ForecastController>>();
             _mockCorrelationContextAccessor = new Mock<ICorrelationContextAccessor>();
+            _mockForecastService = new Mock<IForecastService>();
 
-            _controller = new ForecastController(_mockLogger.Object, _mockCorrelationContextAccessor.Object);
+            _controller = new ForecastController(_mockLogger.Object,
+                _mockCorrelationContextAccessor.Object,
+                _mockForecastService.Object);
         }
 
         [Fact]
@@ -26,12 +32,25 @@ namespace AuditedApi.Controllers.Tests
             // Arrange
             _mockCorrelationContextAccessor.SetupGet(static x => x.CorrelationContext)
                 .Returns(new CorrelationContext("ABC-123", "RequestHeader"));
+            _mockForecastService.Setup(static x => x.GetForecast(It.IsAny<DateTime>()))
+                .Returns(CreateWeatherForecasts());
 
             // Act
             var result = _controller.GetForecasts();
 
             // Assert
-            Assert.Equal(5, result.Length);
+            Assert.Single(result);
+        }
+
+        private static WeatherForecast[] CreateWeatherForecasts()
+        {
+            WeatherForecast forecast = new(
+                DateOnly.FromDateTime(DateTime.Now),
+                20,
+                "Warm"
+            );
+
+            return [forecast];
         }
     }
 }
